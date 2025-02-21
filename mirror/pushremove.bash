e57dade53df84825fd31a95d1a8f7990cf0b721d
Sampsa Riikonen <sampsa.riikonen@amd.com>
2025-02-21 16:57:25 +0200

    etc

--- mirror/pushremove.bash
+++ mirror/pushremove.bash
@@ -9,7 +9,7 @@ rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete --exclude "**/.g
 # rsync --delete -uv $HOME/shared/* $username@$hostname:shared/ & \
 rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git" --include="*.py" --include="*.ipynb" --include="*/" --exclude="*" $HOME/shared/notebook/ $username@$hostname:shared/notebook/ & \
 rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git"--include="*.py" --include="*.ipynb" --include="*/" --exclude="*" $HOME/shared/script/ $username@$hostname:shared/script/ & \
-rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git" --include="*.py" --include="*/" --exclude="*" $HOME/shared/tests/ $username@$hostname:shared/tests/ & \
+rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --include="*.py" --include "*.txt" --include "*.csv" --include "*.json" --include "*.md" --include "*.bash" --include="*/" --exclude="*" $HOME/shared/tests/ $username@$hostname:shared/tests/ & \
 rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git" $HOME/shared/bin/ $username@$hostname:shared/bin/ & \
 rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git" $HOME/shared/env/ $username@$hostname:shared/env/ & \
 rsync -e "ssh -p $sshport -o StrictHostKeyChecking=no" --delete -uvr --exclude "**/.*" --exclude "**/.git" $HOME/shared/pythonenv/* $username@$hostname:shared/pythonenv/ &
